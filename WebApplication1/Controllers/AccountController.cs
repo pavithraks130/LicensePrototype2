e286c653d4c5433927786b8f216cfd60553d3086
kkannan <kkannan@PC-BANG-615E>
2017-02-21 12:56:21 +0530

    Added new projecr

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using License.Core.Manager;
+using License.Logic.ServiceLogic;
+using License.MetCalWeb.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserLogic logic = new UserLogic();
+
+
+        public ActionResult Register()
+        {
+            ViewData["SucessMessageDisplay"] = false;
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (logic.UserManager == null)
+                    logic.UserManager = Request.GetOwinContext().GetUserManager<AppUserManager>();
+                if (logic.RoleManager == null)
+                    logic.RoleManager = Request.GetOwinContext().GetUserManager<AppRoleManager>();
+                IdentityResult result = logic.CreateUser(model.RegistratoinModel);
+                return View("LogIn");
+            }
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using License.Core.DBContext;
+using License.Core.Manager;
+
+
+[assembly: OwinStartup(typeof(WebApplication1.Startup))]
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.CreatePerOwinContext<AppRoleManager>(AppRoleManager.Create);
+        }
+    }
+}
