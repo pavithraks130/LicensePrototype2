6fb56ed653f71269f74367e9eb004eceeb1b8e6f
CShettyFluke <charan.shetty@fluke.com>
2017-02-23 12:56:26 +0530

    Team _tab UI  Design

---
+++ WebApplication1/Controllers/TeamController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: Team
+        public ActionResult TeamContainer()
+        {
+            return View();
+        }
+
+        public ActionResult TeamMembers()
+        {
+            return View();
+        }
+
+        public ActionResult Subscriptions()
+        {
+            return View();
+        }
+    }
+}
