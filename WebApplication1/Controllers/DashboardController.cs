05927f2b319a6df5e6ebe15b5cf7b43a517325de
CShettyFluke <charan.shetty@fluke.com>
2017-02-27 11:09:26 +0530

    Dashboard Logic

--- WebApplication1/Controllers/DashboardController.cs
+++ WebApplication1/Controllers/DashboardController.cs
@@ -32,7 +32,32 @@ namespace License.MetCalWeb.Controllers
         // GET: Dashboard
         public ActionResult Index()
         {
-            return View();
+            var model = new List<Product>();
+            var product = new Product();
+            product.CategoryID = 10;
+            product.ImagePath = "../Catalog/Images/Thumbs/f-3510-fc_01a_h-708x490.png";
+            product.ProductID = 123;
+            product.ProductName = "Calibrator";
+            product.UnitPrice = 499;
+            model.Add(product);
+
+            Product product1 = new Product();
+            product1.CategoryID = 20;
+            product1.ImagePath = "../Catalog/Images/Thumbs/f-3510-fc_01a_h-708x490.png";
+            product1.ProductID = 123;
+            product1.ProductName = "f-1730-hanger";
+            product1.UnitPrice = 600;
+            model.Add(product1);
+
+            Product product3 = new Product();
+            product3.CategoryID = 30;
+            product3.ImagePath = "../Catalog/Images/Thumbs/f-3510-fc_01a_h-708x490.png";
+            product3.ProductID = 123;
+            product3.ProductName = "f-3501-fc";
+            product3.UnitPrice = 588;
+            model.Add(product3);
+
+            return View(model);
         }
 
     }
