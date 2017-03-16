3bb61594ab4346afb8fcdb76afe336e3c5edc5f9
kkannan <kkannan@PC-BANG-615E>
2017-03-16 09:04:20 +0530

    Changes to Desktop app

---
+++ License.MetCalDesktop/Common/BaseEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace License.MetCalDesktop.Common
+{
+   public delegate void NavigateNextPage(string screenName, Dictionary<string, string> additionalInfo);
+
+    /// <summary>
+    /// This calss is used to perform a comman action in all other viewmodel class.
+    /// </summary>
+    public class BaseEntity : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// This is used for page navigation.
+        /// </summary>
+        public NavigateNextPage NavigateNextPage;
+
+        /// <summary>
+        /// This will update UI fields
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// On PropertyChanged event 
+        /// </summary>
+        /// <param name="propertyName">propertyName</param>
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
