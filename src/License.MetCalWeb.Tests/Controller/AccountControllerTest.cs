23c79ff87140fb6f5975ed5f716e1ec6a2d988ae
CShettyFluke <charan.shetty@fluke.com>
2017-03-24 12:36:18 +0530

    Unit test Changes

--- src/License.MetCalWeb.Tests/Controller/AccountControllerTest.cs
+++ src/License.MetCalWeb.Tests/Controller/AccountControllerTest.cs
@@ -6,22 +6,65 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using License.MetCalWeb;
 using System.Web.Mvc;
+using License.MetCalWeb.Controllers;
 
 namespace License.MetCalWeb.Tests.Controller
 {
     [TestClass]
     public class AccountControllerTest
     {
-        [TestMethod]
-        public void RegistrationVerification()
+
+        AccountController accountController = null;
+        #region Initialization
+        /// <summary>
+        /// Test Initializations
+        /// </summary>
+        [TestInitialize]
+        public void Init()
         {
-            Controllers.AccountController accCtrl = new Controllers.AccountController();
-            Models.LoginViewModel viewModel = new Models.LoginViewModel();
-            viewModel.Email = "apsarj@gmail.com";
-            viewModel.Password = "Test@1234";
-            var obj = accCtrl.LogIn(viewModel) as ViewResult;
-            Assert.AreEqual("Home", obj.MasterName);
+            accountController = new AccountController();
+        }
+        #endregion Initialization
+
+        #region Test Methods
+
+        /// <summary>
+        /// Register Http get request from user
+        /// </summary>
+        [TestMethod]
+        public void Register_Http_Get_Test()
+        {
+            var result = accountController.Register() as ViewResult;
+            Assert.AreEqual(false,result.ViewData["SucessMessageDisplay"]);
         }
 
+        /// <summary>
+        /// LogIn user HttpGet Test
+        /// </summary>
+        [TestMethod]
+        public void LogIn_HttpGet_Test()
+        {
+            var result = accountController.LogIn() as ViewResult;
+            Assert.AreEqual(string.Empty,result.ViewName);
+        }
+
+
+        [TestMethod]
+        public void LogOut_Test()
+        {
+            var result = accountController.LogOut() as ViewResult;
+        }
+        #endregion Test Methods
+
+        #region Class CleanUp
+        /// <summary>
+        /// class level object cleanup
+        /// </summary>
+        [TestCleanup]
+        public void CleanUp()
+        {
+            accountController = null;
+        }
+        #endregion Class CleanUp
     }
 }
