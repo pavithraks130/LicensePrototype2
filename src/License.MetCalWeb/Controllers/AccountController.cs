1f2c55ffb1932aa6d2b695d631e06aaeea2ff109
CShettyFluke <charan.shetty@fluke.com>
2017-03-30 14:28:39 +0530

    Bug fix bulk license add

--- src/License.MetCalWeb/Controllers/AccountController.cs
+++ src/License.MetCalWeb/Controllers/AccountController.cs
@@ -157,9 +157,7 @@ namespace License.MetCalWeb.Controllers
                     }
                     else
                         LicenseSessionState.Instance.IsAdmin = LicenseSessionState.Instance.User.Roles.Contains("Admin");
-
-
-                    SubscriLogic.GetUserLicenseForUser();
+                  
                 }
                 SignInAsync(userObj, true);
                 LicenseSessionState.Instance.IsAuthenticated = true;
