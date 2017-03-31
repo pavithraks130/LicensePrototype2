4ec5fa0765988c0202f6304cc53361bafecde095
kkannan <kkannan@PC-BANG-615E>
2017-03-31 11:10:28 +0530

    Fixed issue of committing TeamAssetLogic at wrong place

--- src/License.Logic/ServiceLogic/TeamAssetLogic.cs
+++ src/License.Logic/ServiceLogic/TeamAssetLogic.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using License.Core.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamAssetModel = License.Model.TeamAsset;
 
 namespace License.Logic.ServiceLogic
 {
     class TeamAssetLogic : BaseLogic
     {
-
+        public TeamAssetModel CreateInvite(TeamAssetModel teamAsset)
+        {
+            TeamAsset convertedTeamAsset = AutoMapper.Mapper.Map<TeamAssetModel, TeamAsset>(teamAsset);
+            var obj = Work.TeamAssetRepository.GetData(f => f.SerialNumber == teamAsset.SerialNumber).FirstOrDefault();
+            if (obj == null)
+            {
+                obj = Work.TeamAssetRepository.Create(convertedTeamAsset);
+                Work.TeamAssetRepository.Save();
+            }
+            return AutoMapper.Mapper.Map<TeamAsset, TeamAssetModel>(obj);
+        }
     }
 }
