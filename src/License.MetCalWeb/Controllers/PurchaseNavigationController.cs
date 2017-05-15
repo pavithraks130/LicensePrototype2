b853d621efbfc70a3b3f6be07692d6ab77e4f5ba
CShettyFluke <charan.shetty@fluke.com>
2017-05-15 10:10:50 +0530

    UI Changes

---
+++ src/License.MetCalWeb/Controllers/PurchaseNavigationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace License.MetCalWeb.Controllers
+{
+    public class PurchaseNavigationController : Controller
+    {
+        // GET: PurchaseNavigation
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Shop()
+        {
+            return View();
+        }
+        public ActionResult PayQuote()
+        {
+            return View();
+        }
+        public ActionResult Renew()
+        {
+            return View();
+        }
+        public ActionResult UpGrade()
+        {
+            return View();
+
+        }
+    }
+}
